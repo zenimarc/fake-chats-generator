e84c4efad05f409733d5decbc7a1191f819dbe2e
Marco V <48255395+zenimarc@users.noreply.github.com>
2024-07-15 20:50:05 +0200

    first commit

---
+++ src/lib/components/SystemBar.svelte
@@ -0,0 +1,3 @@
+<section class="bg-android-bar h-[24px] flex items-center">
+	<p class="text-white text-xs mx-auto">Android System Bar</p>
+</section>
---
+++ src/routes/(app)/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import SystemBar from '$lib/components/SystemBar.svelte';
+</script>
+
+<div class="h-dvh flex items-center">
+	<div class="w-[272px] h-[572px] bg-gray-200 mx-auto">
+		<SystemBar />
+		<h1>Welcome to SvelteKit</h1>
+		<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	</div>
+</div>
